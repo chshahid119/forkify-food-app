dc7b8482b0977053acc637b7a00d429c49b8215b
zubairch1199 <133282511+chshahid119@users.noreply.github.com>
2024-02-22 01:03:17 +0500

    add fake hash change functionalities

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,8 @@
 // ALL IMPORTS
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../img/icons.svg'; // Parcel 2
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
 const recipeContainer = document.querySelector('.recipe');
 
@@ -30,12 +32,18 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    //Guard Class
+    if (!id) return;
+
     // 1) Loading Recipe
     renderSpinner(recipeContainer);
 
     const res = await fetch(
       // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc40'
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
 
     const data = await res.json();
@@ -162,6 +170,11 @@ const showRecipe = async function () {
   }
 };
 showRecipe();
+
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
 
 // -----------------------------------------------------------
 // Again Repeating from start
