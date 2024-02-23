beec3bbe69441b0ae8ba31d1ad05405e796f8efa
zubairch1199 <133282511+chshahid119@users.noreply.github.com>
2024-02-24 03:26:49 +0500

    made some changes

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -9,7 +9,6 @@ export default class View {
    * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
    * @returns {undefined | string} A markup string is returned if render=false
    * @this {Object} View instance
-   * @author Jonas Schmedtmann
    * @todo Finish implementation
    */
   render(data, render = true) {
